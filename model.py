00f2e7a9e83ef3a81d0dc8647ca442adf36d6256
minhtannguyen <mn15@rice.edu>
2017-07-19 01:55:41 -0700

    save real hand pose data into a new folder

--- model.py
+++ model.py
@@ -226,7 +226,7 @@ class Model(object):
           #t1 = [[1,2,3],[4,5,6]]
           #t2 = [[7,8,9],[10,11,12]]
           #tf.concat_v2([t1,t2],0) = [[1,2,3],[4,5,6],[7,8,9],[10,11,12]]
-          tf.concat_v2([self.refiner_d_loss, self.refiner_d_loss_with_history], axis=0) + \
+          tf.concat([self.refiner_d_loss, self.refiner_d_loss_with_history], axis=0) + \
               self.synthetic_d_loss, name="discrim_loss_with_history")
 
 
